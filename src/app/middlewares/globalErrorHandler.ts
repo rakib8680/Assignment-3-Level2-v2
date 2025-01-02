23f533f2d796fe287e6e9bd9d425faa2582e4c27
rakib8680 <rakib.cst.1st@gmail.com>
2025-01-02 18:46:38 +0600

    Add handler for Mongoose Cast Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,7 @@ import { ZodError } from "zod";
 import { TErrorResponse } from "../types/TErrorResponse";
 import handleZodError from "../errors/handleZodError";
 import handleValidationError from "../errors/handleValidationError";
+import handleCastError from "../errors/handleCastError";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let errorResponse:TErrorResponse = {
@@ -21,6 +22,8 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorResponse = handleZodError(err);
   }else if(err?.name ==="ValidationError"){
     errorResponse = handleValidationError(err);
+  }else if(err?.name ==="CastError"){
+    errorResponse = handleCastError(err);
   }
 
 
