c0be76574cb50d23802acd21ac974b213bb2de28
rakib8680 <rakib.cst.1st@gmail.com>
2025-01-11 18:11:53 +0600

    Refactored QueryBuilder Class

--- src/app/helpers/queryBuilder.ts
+++ src/app/helpers/queryBuilder.ts
@@ -1,4 +1,4 @@
-import { FilterQuery, Query } from 'mongoose';
+import { FilterQuery, Query } from "mongoose";
 
 class QueryBuilder<T> {
   public modelQuery: Query<T[], T>;
@@ -9,73 +9,63 @@ class QueryBuilder<T> {
     this.query = query;
   }
 
+  // Method to search documents based on a search term in specified fields
   search(searchableFields: string[]) {
-    const searchTerm = this?.query?.searchTerm;
+    const { searchTerm } = this.query;
     if (searchTerm) {
       this.modelQuery = this.modelQuery.find({
         $or: searchableFields.map(
           (field) =>
             ({
-              [field]: { $regex: searchTerm, $options: 'i' },
-            }) as FilterQuery<T>,
+              [field]: { $regex: searchTerm, $options: "i" },
+            }) as FilterQuery<T>
         ),
       });
     }
-
     return this;
   }
 
+  // Method to filter documents based on query parameters, excluding certain fields
   filter() {
-    const queryObj = { ...this.query }; // copy
-
-    // Filtering
-    const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
-
+    const queryObj = { ...this.query };
+    const excludeFields = ["searchTerm", "sort", "limit", "page", "fields"];
     excludeFields.forEach((el) => delete queryObj[el]);
-
     this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
-
     return this;
   }
 
+  // Method to sort documents based on a sort parameter or default to '-createdAt'
   sort() {
     const sort =
-      (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
-    this.modelQuery = this.modelQuery.sort(sort as string);
-
+      (this.query.sort as string)?.split(",").join(" ") || "-createdAt";
+    this.modelQuery = this.modelQuery.sort(sort);
     return this;
   }
 
+  // Method to paginate the results based on page and limit parameters
   paginate() {
-    const page = Number(this?.query?.page) || 1;
-    const limit = Number(this?.query?.limit) || 10;
+    const page = Number(this.query.page) || 1;
+    const limit = Number(this.query.limit) || 10;
     const skip = (page - 1) * limit;
-
     this.modelQuery = this.modelQuery.skip(skip).limit(limit);
-
     return this;
   }
 
+  // Method to select specific fields to be returned in the results
   fields() {
-    const fields =
-      (this?.query?.fields as string)?.split(',')?.join(' ') || '-__v';
-
+    const fields = (this.query.fields as string)?.split(",").join(" ") || "-__v";
     this.modelQuery = this.modelQuery.select(fields);
     return this;
   }
+
+  // Method to count the total number of documents matching the query
   async countTotal() {
     const totalQueries = this.modelQuery.getFilter();
     const total = await this.modelQuery.model.countDocuments(totalQueries);
-    const page = Number(this?.query?.page) || 1;
-    const limit = Number(this?.query?.limit) || 10;
+    const page = Number(this.query.page) || 1;
+    const limit = Number(this.query.limit) || 10;
     const totalPage = Math.ceil(total / limit);
-
-    return {
-      page,
-      limit,
-      total,
-      totalPage,
-    };
+    return { page, limit, total, totalPage };
   }
 }
 
