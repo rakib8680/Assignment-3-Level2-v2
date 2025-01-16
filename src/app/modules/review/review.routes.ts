dec4e273261563831c30ad81f58140b26d1c12ef
rakib8680 <rakib.cst.1st@gmail.com>
2025-01-16 12:52:32 +0600

    Add review route and request validation middleware when creating reviews

--- src/app/modules/review/review.routes.ts
+++ src/app/modules/review/review.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { reviewController } from "./review.controller";
+import requestValidation from "../../middlewares/requestValidation";
+import { reviewValidations } from "./review.validation";
 
 const router = Router();
 
-
-router.post('/create-review', reviewController.createReview)
-router.get('/all-reviews', reviewController.getAllReviews)
-
-
-
+router.post(
+  "/create-review",
+  requestValidation(reviewValidations.createReviewValidationSchema),
+  reviewController.createReview
+);
+router.get("/all-reviews", reviewController.getAllReviews);
 
 export const ReviewRoutes = router;
